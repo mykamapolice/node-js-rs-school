674463d0a355161f7ec627ada8aa33ec0c774b7a
Ilya <ilya.barachenia@myaic.com>
2021-11-17 08:42:37 +0100

    create app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const {argv, stdout} = process
+const {argv, stdout, stdin} = process
 
 const indexOfInput = argv.indexOf('-i') + 1
 const indexOfOutput = argv.indexOf('-o') + 1
@@ -9,5 +9,120 @@ const indexOfConfig= argv.indexOf('-c') + 1
 const input = argv[indexOfInput]
 const output = argv[indexOfOutput]
 const config = argv[indexOfConfig]
+
+function isLetter(c) {
+  return c.toLowerCase() !== c.toUpperCase();
+}
+
+if(!config) throw Error('No config found, please enter config after "-c"')
+
+const inputStream = fs.createReadStream(input, 'utf-8');
+const writeStream = fs.createWriteStream(output);
+
+let data = ''
+let outputData = ''
+
+inputStream.on('data', chunk => data += chunk);
+
+inputStream.on('end', () => {
+  data.split('').map(letter => {
+    if(isLetter(letter)) {
+      outputData += converter(config, letter)
+    } else {
+      outputData += letter
+    }
+  })
+  writeStream.write(outputData)
+})
+
+inputStream.on('error', () => {
+  stdout.write('Enter text to convert: \n')
+  stdin.on('data', input => {
+    input.toString().trim().split('').map(letter => {
+      if(isLetter(letter)) {
+        outputData += converter(config, letter)
+      } else {
+        outputData += letter
+      }
+    })
+    writeStream.write(outputData)
+  })
+});
+
+
+writeStream.on('error', () => {
+  outputData.toString().trim().split('').map(letter => {
+    if(isLetter(letter)) {
+      stdout.write(converter(config, letter))
+    } else {
+      stdout.write(letter)
+    }
+  })
+})
+
+function converter(config, letter) {
+  const conf = config.split('-')
+  let a = letter
+  conf.map(el => {
+    switch (el) {
+      case 'C1':
+        a = ceasarCipher(a)
+        break
+      case 'C0':
+        a = ceasarCipher(a, true)
+        break
+      case 'A':
+        a = atbashCipher(a)
+        break
+      case 'R1':
+        a = Rot8Cipher(a)
+        break
+      case 'R0':
+        a = Rot8Cipher(a, true)
+        break
+    }
+  })
+  return a
+}
+
+
+function ceasarCipher(n, decode = false) {
+  const a = n.charCodeAt(0)
+  if(!decode) {
+    if(n.toLowerCase() === 'z' ){
+      return String.fromCharCode((a + 1) - 26);
+    }
+    return String.fromCharCode((a + 1));
+  } else {
+    if(n.toLowerCase() === 'a' ){
+      return String.fromCharCode((a - 1) + 26);
+    }
+    return String.fromCharCode((a - 1));
+  }
+}
+
+function Rot8Cipher(n, decode = false) {
+  const a = n.charCodeAt(0)
+  if(!decode) {
+    if(n.toLowerCase() > 'r' ){
+      return String.fromCharCode((a + 8) - 26);
+    }
+    return String.fromCharCode((a + 8));
+  } else {
+    if(n.toLowerCase() < 'h' ){
+      return String.fromCharCode((a - 8) + 26);
+    }
+    return String.fromCharCode((a - 8));
+  }
+}
+
+function atbashCipher(n) {
+  const a = n.charCodeAt(0)
+  if(n === n.toLowerCase()) {
+    return String.fromCharCode(97 + (122 - a))
+  } else {
+    return String.fromCharCode(65 + (90 - a))
+  }
+}
 
 
